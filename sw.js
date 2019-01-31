7be9a2c9b98d56c05cefd55ac5c85bf9bb52ac96
Fredrik B <fredbouk@gmail.com>
2019-01-31 13:37:41 +0100

    Added options object to cache.match() to find a match for every restaurant

--- sw.js
+++ sw.js
@@ -18,17 +18,7 @@ self.addEventListener('install', function (event) {
   'js/main.js',
   'js/restaurant_info.js',
   'index.html',
-  'restaurant.html',
-  'restaurant.html?id=1',
-  'restaurant.html?id=2',
-  'restaurant.html?id=3',
-  'restaurant.html?id=4',
-  'restaurant.html?id=5',
-  'restaurant.html?id=6',
-  'restaurant.html?id=7',
-  'restaurant.html?id=8',
-  'restaurant.html?id=9',
-  'restaurant.html?id=10']
+  'restaurant.html']
   
   event.waitUntil(
 		caches.open('rrApp-static').then(function (cache) {
@@ -39,7 +29,7 @@ self.addEventListener('install', function (event) {
 
 self.addEventListener('fetch', function (event) {
 	event.respondWith(
-		caches.match(event.request).then(function (response) {
+		caches.match(event.request, {ignoreSearch: true}).then(function (response) {
 			if (response) return response;
 			return fetch(event.request);
 		})
